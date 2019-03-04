c0b60399d26c3d78952e39f8d93d534676d25816
wyvonj <wyvonj@gmail.com>
2019-03-04 17:21:06 +0800

    initial commit with basic rn-ts configuration

---
+++ rn-cli.config.js
@@ -0,0 +1,9 @@
+// 包含对React Native Typescript Transformer的设置
+module.exports = {
+  getTransformModulePath() {
+    return require.resolve('react-native-typescript-transformer');
+  },
+  getSourceExts() {
+    return ['ts', 'tsx'];
+  }
+}
